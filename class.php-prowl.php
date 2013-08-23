1b880e9b5f4341bc84de417304c62c682cea7718
Callum R. Jones <cj@icj.me>
2013-08-23 16:14:15 +0100

    fix "Use of undefined constant DEMO_EOL" notice

--- class.php-prowl.php
+++ class.php-prowl.php
@@ -49,7 +49,7 @@ class Prowl
 	}
 	public function setDebug($bool) {
 		$this->debug = $bool;
-		if (!defined(DEMO_EOL)) {
+		if (!defined('DEMO_EOL')) {
 			define("DEMO_EOL",isset($_SERVER['HTTP_USER_AGENT']) ? "<br />" : "\n");
 		}
 	}
